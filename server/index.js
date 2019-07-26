72af22d5501f4c380854494103328af1a89c5778
Luis Rangel Castro <luisrangelc@rpaipos.com>
2019-07-25 20:16:35 -0600

    first commit local

---
+++ server/index.js
@@ -0,0 +1,16 @@
+import http from 'http';
+import Debug from 'debug';
+
+const PORT = 3000;
+const debug = new Debug('platzioverflowbe:root');
+
+const app = http.createServer((req, res) => {
+    debug('New request');
+    res.writeHead(200, { 'Content-Type': 'text/plain' });
+    res.write('Hola desde el servidor yeap');
+    res.end();
+});
+
+app.listen(PORT, () => {
+    debug(`Server running at port ${PORT} KORN`);
+})
