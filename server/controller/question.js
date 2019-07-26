02220cf393d98cda223d3968194853f62e8618ff
Luis Rangel Castro <luisrangelc@rpaipos.com>
2019-07-25 23:30:16 -0600

    DESAFIO: Contruye el spinner del detalle de la pregunta

--- server/controller/question.js
+++ server/controller/question.js
@@ -1,6 +1,6 @@
 const question = {
 	_id: 1,
-	title: 'Holaaaa ¿Cómo reutilizo un componente en Android?',
+	title: '¿Cómo reutilizo un componente en Android?',
 	description: 'Miren esta es mi pregunta',
 	createdAt: new Date(),
 	icon: 'devicon-android-plain',
@@ -19,11 +19,16 @@ function getQuestions(req, res) {
 	// setTimeout(() => {
 	// 	res.status(200).json(questions);
 	// }, 2000);
+
 	res.status(200).json(questions);
 }
 
 function getQuestion(req, res) {
-	res.status(200).json(question);
+	setTimeout(() => {
+		res.status(200).json(question);
+	}, 2000);
+
+	// res.status(200).json(question);
 }
 
 export default {
